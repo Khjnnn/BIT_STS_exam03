da8742470d1f882b0993a9fdcf7771d79a472624
khj <hoyun7167@naver.com>
2020-03-04 00:08:44 +0900

    Spring exam

--- hj03/src/main/java/org/khen/dto/PageDTO.java
+++ hj03/src/main/java/org/khen/dto/PageDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class PageDTO {
+	//페이지 정보 클래스
 	
 	private int page; //페이지번호
 	private int amount; // 페이지당 최대갯수
--- hj03/src/main/java/org/khen/dto/PageUtil.java
+++ hj03/src/main/java/org/khen/dto/PageUtil.java
@@ -6,26 +6,27 @@ import lombok.ToString;
 @Getter
 @ToString
 public class PageUtil {
-	
-	private PageDTO dto;
-	private int total, start, end;
-	private boolean prev, next;
+	// 페이지 유틸리티 클래스
+
+	private PageDTO dto; //페이지 정보 (페이지번호,최대갯수)
+	private int total, start, end; // 총합, 시작번호, 끝번호
+	private boolean prev, next; // 이전 , 다음 (boolean)
 	
 	public PageUtil(PageDTO dto, int total) {
 		this.dto = dto;
 		this.total = total;
 		
-		int pageNum = dto.getPage();
-		int tempEnd = (int)Math.ceil(pageNum/10.0) * 10;
-		this.start = tempEnd - 9 < 0 ? 1:tempEnd - 9 ;
-		this.prev = this.start != 1 ;
+		int pageNum = dto.getPage(); // 페이지번호 가져옴
+		int tempEnd = (int)Math.ceil(pageNum/10.0) * 10; // 가져온 페이지 번호에 대한 마지막 페이지 번호(10페이지 단위)
+		this.start = tempEnd - 9 < 0 ? 1:tempEnd - 9 ; // 페이지 끝번호 - 9 > 음수면 1,아니면 -9한 값
+		this.prev = this.start != 1 ; // 스타트가 1이 아닐경우 false  
 		
-		int realEnd = (int)(total/10.0);
-		this.next = realEnd > tempEnd;
+		int realEnd = (int)(total/10.0); // 전체 페이지에 대한 끝번호 값 
+		this.next = realEnd > tempEnd; // 전체 끝 이 해당페이지 끝번호보다 클경우 true (다음페이지 이동)
 		if(realEnd > tempEnd) {
-			this.end = tempEnd;
+			this.end = tempEnd; // true > 전체 끝번호
 		}else {
-			this.end = realEnd + 1 ;
+			this.end = realEnd + 1 ; // false > 현재 끝번호 +1
 		}
 	}
 	
